383446e307768084be6687725441ec3e38454b3f
Zach Vickery <zvickery@gmail.com>
2016-10-04 00:40:57 -0600

    Add files for running the proxy

--- run/run.sh
+++ run/run.sh
@@ -5,12 +5,11 @@ export LOCAL_IP=127.0.0.1
 export PUBLIC_IP=127.0.0.1
 
 docker run \
-  -it \
+  -d \
   -p ${LOCAL_IP}:80:80 \
   -p ${LOCAL_IP}:443:443 \
   -p ${LOCAL_IP}:53:53/udp \
   -e "CLIENT_IP=${CLIENT_IP}" \
   -e "LOCAL_IP=${LOCAL_IP}" \
   -e "PUBLIC_IP=${PUBLIC_IP}" \
-  --entrypoint bash \
   zdvickery/dns-proxy
